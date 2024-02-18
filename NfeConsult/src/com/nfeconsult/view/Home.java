cfc6ed157b8eef01af2abd21b8f7e174965c677e
Thiago <Thiago@DESKTOP-H3M96SH>
2024-02-18 18:35:54 -0300

    implementado os botoes 'selecionar pasta', 'adicionar', 'excluir';

--- NfeConsult/src/com/nfeconsult/view/Home.java
+++ NfeConsult/src/com/nfeconsult/view/Home.java
@@ -5,12 +5,26 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import com.nfeconsult.controller.NfeController;
+import com.nfeconsult.model.NfeModel;
+
 import javax.swing.JTextField;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.ArrayList;
 
 public class Home extends JFrame {
 
@@ -19,6 +33,10 @@ public class Home extends JFrame {
 	private JTextField txtPathDir;
 	private JTextField txtAddFilter;
 	private JTable tabNfeList;
+
+	private DefaultListModel<String> listModelProduct;
+	private Integer listIndexSelected;
+	private ArrayList<NfeModel> nfeList;
 
 	/**
 	 * Launch the application.
@@ -43,64 +61,130 @@ public class Home extends JFrame {
 		setResizable(false);
 		setTitle("thiagobritto.github.io - NfeConsult - v1.0.0");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 470, 305);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 434, 261);
+		panel.setBounds(0, 0, 454, 261);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		txtPathDir = new JTextField();
-		txtPathDir.setBounds(10, 11, 244, 20);
+		txtPathDir.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtPathDir.setBounds(10, 10, 244, 23);
 		panel.add(txtPathDir);
 		txtPathDir.setColumns(10);
-		
-		JButton btnSelectPathDir = new JButton("Selecione a pasta");
-		btnSelectPathDir.setBounds(264, 10, 160, 23);
+
+		JButton btnSelectPathDir = new JButton("Selecione uma pasta");
+		btnSelectPathDir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser();
+				chooser.setDialogTitle("Selecione uma pasta");
+				chooser.setApproveButtonText("Selecionar pasta");
+				chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+				if (chooser.showOpenDialog(panel) == JFileChooser.APPROVE_OPTION) {
+					txtPathDir.setText(chooser.getSelectedFile().getPath());
+				} else {
+					JOptionPane.showMessageDialog(panel, "Nenhum diretorio foi selecionado", "Aviso",
+							JOptionPane.WARNING_MESSAGE);
+				}
+			}
+		});
+		btnSelectPathDir.setFont(new Font("Arial", Font.BOLD, 11));
+		btnSelectPathDir.setBounds(264, 10, 180, 23);
 		panel.add(btnSelectPathDir);
-		
-		JLabel lblNewLabel = new JLabel("Busca por produtos");
-		lblNewLabel.setBounds(10, 43, 108, 14);
+
+		JLabel lblNewLabel = new JLabel("Busca por produto:");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 11));
+		lblNewLabel.setBounds(11, 48, 108, 14);
 		panel.add(lblNewLabel);
-		
-		JButton btnAddFilter = new JButton("adicionar");
-		btnAddFilter.setBounds(264, 39, 75, 23);
+
+		ActionListener addProductEvent = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String prodName = txtAddFilter.getText().trim().toUpperCase();
+				if (!listModelProduct.contains(prodName) && !prodName.isEmpty())
+					listModelProduct.addElement(prodName);
+				txtAddFilter.setText("");
+				txtAddFilter.requestFocus();
+			}
+		};
+
+		JButton btnAddFilter = new JButton("Adicionar");
+		btnAddFilter.addActionListener(addProductEvent);
+		btnAddFilter.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnAddFilter.setBounds(264, 43, 90, 23);
 		panel.add(btnAddFilter);
-		
+
 		JButton btnRemoveFilter = new JButton("Excluir");
-		btnRemoveFilter.setBounds(349, 39, 75, 23);
+		btnRemoveFilter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (listIndexSelected != null) {
+					listModelProduct.removeElementAt(listIndexSelected);
+					listIndexSelected = null;
+				}
+			}
+		});
+		btnRemoveFilter.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnRemoveFilter.setBounds(364, 43, 80, 23);
 		panel.add(btnRemoveFilter);
-		
+
 		txtAddFilter = new JTextField();
-		txtAddFilter.setBounds(110, 39, 144, 20);
+		txtAddFilter.addActionListener(addProductEvent);
+		txtAddFilter.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtAddFilter.setBounds(120, 43, 134, 23);
 		panel.add(txtAddFilter);
 		txtAddFilter.setColumns(10);
-		
+
 		JScrollPane scrollPaneFilters = new JScrollPane();
-		scrollPaneFilters.setBounds(10, 71, 414, 59);
+		scrollPaneFilters.setBounds(10, 76, 434, 60);
 		panel.add(scrollPaneFilters);
-		
-		JList listFilters = new JList();
+
+		listModelProduct = new DefaultListModel<String>();
+
+		JList<String> listFilters = new JList<String>(listModelProduct);
+		listFilters.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				listIndexSelected = listFilters.getSelectedIndex();
+			}
+		});
+		listFilters.setFont(new Font("Arial", Font.PLAIN, 11));
 		scrollPaneFilters.setViewportView(listFilters);
-		
+
 		JButton btnFind = new JButton("Buscar");
-		btnFind.setBounds(349, 137, 75, 23);
+		btnFind.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					String path = txtPathDir.getText();
+					nfeList = NfeController.getList(path);
+					if (nfeList!=null) {
+						System.out.println(nfeList.size());
+					}
+				} catch (Exception e2) {
+					System.out.println(e2.getMessage());
+				}
+			}
+		});
+		btnFind.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnFind.setBounds(369, 231, 75, 23);
 		panel.add(btnFind);
-		
+
 		JButton btnGenerateCSV = new JButton("Gerar CSV");
-		btnGenerateCSV.setBounds(10, 137, 89, 23);
+		btnGenerateCSV.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnGenerateCSV.setEnabled(false);
+		btnGenerateCSV.setBounds(10, 231, 89, 23);
 		panel.add(btnGenerateCSV);
-		
+
 		JScrollPane scrollPaneNfe = new JScrollPane();
-		scrollPaneNfe.setBounds(10, 168, 414, 80);
+		scrollPaneNfe.setBounds(10, 146, 434, 75);
 		panel.add(scrollPaneNfe);
-		
+
 		tabNfeList = new JTable();
 		scrollPaneNfe.setViewportView(tabNfeList);
 	}

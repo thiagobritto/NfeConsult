af2d334576a191a1d4c753c0700197dc9c951803
Thiago <Thiago@DESKTOP-H3M96SH>
2024-02-18 18:33:54 -0300

    adicionado uma verificação na lista de arquivos

--- NfeConsult/src/com/nfeconsult/service/PathService.java
+++ NfeConsult/src/com/nfeconsult/service/PathService.java
@@ -6,31 +6,28 @@ import java.util.ArrayList;
 
 public class PathService {
 	
-	public static ArrayList<String> findFilesXML(String dir) throws IOException {
-		File dirPath = new File(dir);
-
-		if(!dirPath.isDirectory())
-				throw new IOException("Diretorio m~so encontrsdo!");
+	public static ArrayList<String> findFilesXML(File dirPath) {
 		
 		ArrayList<String> listPath = new ArrayList<String>();
 		
-		for(File x: dirPath.listFiles()) 
-			if(x.getName().endsWith(".xml")) listPath.add(x.getPath());
+		File[] listFiles = dirPath.listFiles();
+		if(listFiles != null)
+			for(File x: listFiles) 
+				if(x.getName().endsWith(".xml")) listPath.add(x.getPath());
 	
 		return listPath;
 	}
 	
-	public static ArrayList<String> findFilesXMLr(String dir) throws IOException {
-		File dirPath = new File(dir);
-
-		if(!dirPath.isDirectory())
-				throw new IOException("Diretorio m~so encontrsdo!");
+	public static ArrayList<String> findFilesXMLr(File dirPath) {
 		
 		ArrayList<String> listPath = new ArrayList<String>();
 		
-		for(File x: dirPath.listFiles()) {
-			if(x.getName().endsWith(".xml")) listPath.add(x.getPath());
-			if(x.isDirectory()) listPath.addAll(findFilesXMLr(x.getPath()));
+		File[] listFiles = dirPath.listFiles();
+		if(listFiles != null) {			
+			for(File x: listFiles) {
+				if(x.getName().endsWith(".xml")) listPath.add(x.getPath());
+				if(x.isDirectory()) listPath.addAll(findFilesXMLr(new File(x.getPath())));
+			}
 		}
 		return listPath;
 	}

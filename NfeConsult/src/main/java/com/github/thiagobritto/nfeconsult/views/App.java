e517dd87012b4a9021da72a876799426e5866924
Thiago <thiagobritto.developer@gmail.com>
2024-02-22 19:12:07 -0300

    start v1.0.0;

---
+++ NfeConsult/src/main/java/com/github/thiagobritto/nfeconsult/views/App.java
@@ -0,0 +1,13 @@
+package com.github.thiagobritto.nfeconsult.views;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
